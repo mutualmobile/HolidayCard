04e79a6b7b7e68199eb7819374a96c5966beb15e
Conrad Stoll <conrad.stoll@mutualmobile.com>
2015-12-08 12:02:55 -0600

    Re-organizing student playground

--- Student.playground/section-1.swift
+++ Student.playground/section-1.swift
@@ -4,22 +4,13 @@ import UIKit
 import CardBuilder
 
 
-// Example Color Options
-let blueSky = Color.BlueSky
-
-// Example Font Options
-let abrilFatFace = Font.AbrilFatFace
-
-// Example Clip Art Options
-let dradle = ClipArt.Dradle
-
 // Create your card
 var card = Card()
 
-// Start here
 // Customize your card
 card.addBackgroundColor(Color.HollyGreen)
 
+// Start here
 
 
 
